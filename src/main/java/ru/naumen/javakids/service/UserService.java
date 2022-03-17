62a1bae775d57ce5cf078f7693e2be1e918b2beb
ZhukovAlV <Olenka2012$github>
2022-03-17 19:01:42 +0600

    Добавлен Spring Security

---
+++ src/main/java/ru/naumen/javakids/service/UserService.java
@@ -0,0 +1,25 @@
+package ru.naumen.javakids.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.naumen.javakids.domain.User;
+import ru.naumen.javakids.repository.UserRepo;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+}
