9f010ec709ad82f2f8ecb55f7afd895c4710a9cb
ZhukovAlV <Olenka2012$github>
2022-04-02 14:49:44 +0600

    Убраны с закомментированные блоки

--- src/main/java/ru/naumen/javakids/configurations/SpringSecurityConfig.java
+++ src/main/java/ru/naumen/javakids/configurations/SpringSecurityConfig.java
@@ -3,14 +3,10 @@ package ru.naumen.javakids.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import ru.naumen.javakids.services.UserService;
 
 /**
@@ -19,7 +15,6 @@ import ru.naumen.javakids.services.UserService;
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -48,11 +43,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             .and().
                 csrf().disable();
     }
-
-/*    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }*/
-
 }
