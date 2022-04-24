032a49fa1064725711bc99a8936862499fdef5f5
ZhukovAlV <Olenka2012$github>
2022-04-25 00:31:02 +0600

    Добавляем админа по умолчанию в базу данных с логином admin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,17 @@
+-- Добавляем админа по умолчанию с одинаковым логином и паролем
+INSERT INTO public.user_table(id, active, email, password, username)
+SELECT COALESCE((SELECT MAX(id) + 1 FROM public.user_table),1) AS id,
+'TRUE', 'admin@javakids.ru', '$2a$10$sTxqUmqAl0OLHCo/vUXVXO69A0UWH1mMtByR.XnHZaMD7yS3ijDCe', 'admin'
+WHERE NOT EXISTS (SELECT id
+	FROM public.user_table
+	WHERE username = 'admin' LIMIT 1);
+
+INSERT INTO public.user_role
+SELECT (SELECT id FROM public.user_table WHERE username = 'admin') AS id, 'ROLE_ADMIN'
+WHERE NOT EXISTS (SELECT user_id FROM public.user_role WHERE user_id = (SELECT id FROM public.user_table WHERE username = 'admin'));
+
+
+/*
 INSERT INTO public.lectures(
 	id, content, topic)
 	VALUES (1, 'Содержимое лекции 1',  'Знакомство с Java');
@@ -9,3 +23,4 @@ INSERT INTO public.lectures(
 INSERT INTO public.lectures(
 	id, content, topic)
 	VALUES (3, 'Содержимое лекции 3', 'Объявление переменных');
+*/
