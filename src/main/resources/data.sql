fffe6a94e254b9c31d461c34fefa7448f4294929
ZhukovAlV <Olenka2012$github>
2022-04-25 11:40:07 +0600

    Внесены изменения в data.sql, добавляются лекции по умолчанию

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,17 +10,15 @@ INSERT INTO public.user_role
 SELECT (SELECT id FROM public.user_table WHERE username = 'admin') AS id, 'ROLE_ADMIN'
 WHERE NOT EXISTS (SELECT user_id FROM public.user_role WHERE user_id = (SELECT id FROM public.user_table WHERE username = 'admin' LIMIT 1));
 
+-- Добавляем лекции, если их нет
+INSERT INTO public.lectures(id, content, topic)
+SELECT 1, 'Содержимое лекции 1',  'Знакомство с Java'
+WHERE NOT EXISTS (SELECT id	FROM public.lectures WHERE id = 1 OR topic = 'Знакомство с Java' LIMIT 1);
 
-/*
-INSERT INTO public.lectures(
-	id, content, topic)
-	VALUES (1, 'Содержимое лекции 1',  'Знакомство с Java');
+INSERT INTO public.lectures(id, content, topic)
+SELECT 2, 'Содержимое лекции 2',  'Структура Java приложения'
+WHERE NOT EXISTS (SELECT id	FROM public.lectures WHERE id = 2 OR topic = 'Структура Java приложения' LIMIT 1);
 
-INSERT INTO public.lectures(
-	id, content, topic)
-	VALUES (2, 'Содержимое лекции 2', 'Структура Java приложения');
-
-INSERT INTO public.lectures(
-	id, content, topic)
-	VALUES (3, 'Содержимое лекции 3', 'Объявление переменных');
-*/
+INSERT INTO public.lectures(id, content, topic)
+SELECT 3, 'Содержимое лекции 3',  'Объявление переменных'
+WHERE NOT EXISTS (SELECT id	FROM public.lectures WHERE id = 3 OR topic = 'Объявление переменных' LIMIT 1);
