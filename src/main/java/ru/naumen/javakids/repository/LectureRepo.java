446380cc930b033dfc07bc72ba248adcd8cf75f2
ZhukovAlV <Olenka2012$github>
2022-05-10 16:19:33 +0600

    Добавлены тесты

--- src/main/java/ru/naumen/javakids/repository/LectureRepo.java
+++
@@ -1,7 +0,0 @@
-package ru.naumen.javakids.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.naumen.javakids.model.Lecture;
-
-public interface LectureRepo extends CrudRepository<Lecture, Long> {
-}
